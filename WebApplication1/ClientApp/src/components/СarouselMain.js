1f569f5e3a7a70280bdd95f90a809715a66e52b1
Kirill <Kirill@DESKTOP-OIQNT93>
2019-06-23 11:41:02 +0300

    😆🤔✨

---
+++ WebApplication1/ClientApp/src/components/СarouselMain.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Button from "rambler-ui/Button";
+
+class Сarousel extends Component {
+    render() {
+        return (
+                <div>
+                    <div className={'discr'} style={
+                        {marginLeft: '14%',
+                            marginBottom: "2%"
+                            ,height: '60vh', width: '72vw',borderRadius: 20 }}
+                    > <h1>Привет мир!</h1>
+                        <p  style={{paddingRight: "40%",fontSize: '2.1vh' }}>
+                            Наконецто сайт  работает с некоотрыми
+                            нюансами но все же ;) и вот вам немного воды:
+                            выстроенный бесконечный канон с полизеркальной векторно-голосовой
+                            структурой,Соноропериод, в первом приближении, имеет миксолидийский алеаторически
+                        </p>
+                        <Button style={{color: "#ffff",marginTop: "1vh" }} size={"small"} type={'primary'} rounded={true}>
+                            Read more
+                        </Button>
+                    </div>
+                    <img src={'../marc-kargel-1621473-unsplash.jpg'} style={
+                        {marginLeft: '14%',
+                            filter: 'brightness(0.6)',
+                            marginBottom: "2%"
+                            ,height: '60vh', width: '72vw',borderRadius: 20 }}/>
+                </div>
+        );
+    }
+}
+
+export default Сarousel;
