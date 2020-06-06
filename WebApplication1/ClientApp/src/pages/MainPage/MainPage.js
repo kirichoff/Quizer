dd0f7567931a571b243e56022f028c9b963ddb09
kkd <kkd@omegasoftware.eu>
2020-06-06 14:58:27 +0300

    stepa sosat 4 bez registatsii i sms 🐓 🌈33

--- WebApplication1/ClientApp/src/pages/MainPage/MainPage.js
+++ WebApplication1/ClientApp/src/pages/MainPage/MainPage.js
@@ -3,18 +3,26 @@ import './MainPage.css'
 import Menu from "../../components/Menu";
 import userHelper from "../../utils/userHelper";
 import Link from "react-router-dom/Link";
+import Button from "rambler-ui/Button";
 
 function MainPage(props) {
     let user = userHelper.GetUser()
 
     return (<div>
-        <Menu user={user} {...props} />
+            <Menu user={user} {...props} />
             <div className={'bg-main'}>
 
                 <Link to={'/bg/LoginBeta'}>
                     Войти
                 </Link>
 
+                <Link to={'/bg/LoginBeta'}>
+                    <Button>
+                        Войти
+                    </Button>
+                </Link>
+
+                <div>Добро ПОжаловать</div>
 
             </div>
         </div>
